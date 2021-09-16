7e9d42268e06941a8ddb3b031491fa6717b6340a
LinQingWei <1441779217@qq.com>
2021-09-16 21:15:44 +0800

    build(all): update build gradle config

--- Simple-Commons/src/main/java/com/github/simple/commons/widget/MyEditText.kt
+++ Simple-Commons/src/main/java/com/github/simple/commons/widget/MyEditText.kt
@@ -8,12 +8,12 @@ import com.github.simple.commons.extension.applyColorFilter
 
 class MyEditText : AppCompatEditText {
 
-    constructor(context: Context?) : super(context)
+    constructor(context: Context?) : super(context!!)
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs)
 
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int)
-            : super(context, attrs, defStyleAttr)
+            : super(context!!, attrs, defStyleAttr)
 
     fun setColors(textColor: Int, accentColor: Int, backgroundColor: Int) {
         background?.mutate()?.applyColorFilter(accentColor)
--- Simple-Commons/src/main/java/com/github/simple/commons/widget/MyTextView.kt
+++ Simple-Commons/src/main/java/com/github/simple/commons/widget/MyTextView.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.widget.AppCompatTextView
 
 class MyTextView : AppCompatTextView {
 
-    constructor(context: Context?) : super(context)
+    constructor(context: Context?) : super(context!!)
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs)
 
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr)
 
     fun setColors(textColor: Int, accentColor: Int) {
         setTextColor(textColor)
